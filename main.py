c56625117cd4ae992a7c3982c7443a29ecd21027
Kartik Dua <kartik.dua@icloud.com>
2024-04-22 12:21:46 +0530

    Add files via upload
    I have used my own test images , I you want to use it please change it respectively

---
+++ main.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'Training images'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+# Maintain a list of names for which attendance has been marked
+markedAttendance = []
+
+def markAttendance(name, filename):
+    if name not in markedAttendance:
+        with open(filename, 'a+') as f:
+            now = datetime.now()
+            dtString = now.strftime("%m/%d/%Y,%H:%M:%S")
+            f.writelines(f'\n{name},{dtString}')
+        markedAttendance.append(name)
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+try:
+    filename_date = datetime.now().strftime("Attendance_%Y-%m-%d.csv")
+    while True:
+        success, img = cap.read()
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            matchIndex = np.argmin(faceDis)
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                markAttendance(name, filename_date)
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
+except KeyboardInterrupt:
+    print("Program interrupted. Exiting gracefully.")
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
